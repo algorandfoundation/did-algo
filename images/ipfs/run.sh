232bf20720c3b4ba20fb20af21180a4d9eeb7819
Ben Cessa <ben@pixative.com>
2022-06-07 10:44:30 -0500

    IPFS node v0.12.0

--- images/ipfs/run.sh
+++ images/ipfs/run.sh
@@ -8,15 +8,10 @@ if [ ! -f /var/lib/ipfs/config ]; then
   ipfs init --profile server
 fi
 
-# CORS domains ca be provided as a comma separated list of
-# string. If no value is provided, a single "*" entry is used
-# by default.
-IPFS_API_CORS=${IPFS_API_CORS:-'"*"'}
-
 # Make sure API and Gateway are accessible outside the container.
 ipfs config Addresses.API /ip4/0.0.0.0/tcp/5001
 ipfs config Addresses.Gateway /ip4/0.0.0.0/tcp/8080
-ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '[${IPFS_API_CORS}]'
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
 ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "POST"]'
 
 
