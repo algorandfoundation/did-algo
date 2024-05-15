328ae5b6b3bf62e2a91d273749c5ebd888b64323
Joe Polny <joepolny@gmail.com>
2024-05-15 11:14:26 -0400

    chore: rm unused heml and images directory

--- images/algoid-resolver/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Automatically exit on error
-set -e
-
-# Use 'exec' so that the 'algoid' application becomes the container’s
-# PID 1. This allows the application to receive any Unix signals sent
-#to the container.
-exec algoid resolver
